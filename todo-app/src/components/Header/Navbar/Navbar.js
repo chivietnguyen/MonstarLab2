eee258ed94857cbfcdc9bad9981b3761ad6956e7
Nguyen Chi Viet <nguyenchiviet.cv@gmail.com>
2022-10-12 09:31:29 +0700

    finished test git pull

--- todo-app/src/components/Header/Navbar/Navbar.js
+++ todo-app/src/components/Header/Navbar/Navbar.js
@@ -5,8 +5,6 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
 	return (
 		<div className={styles.navbar}>
-			{console.log("???")}
-			{console.log("Nguyễn Chí Việt")}
 			<div className={styles.user}>
 				<div className={styles.userAvatar}>
 					<img
