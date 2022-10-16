80b4dc16427df6eba33e6a2ef8ce12648596ab75
Nguyen Chi Viet <nguyenchiviet.cv@gmail.com>
2022-10-16 23:38:17 +0700

    Modify UI for TodoCard

--- todo-app/src/components/Todolist/TodoBody/TodoBody.js
+++ todo-app/src/components/Todolist/TodoBody/TodoBody.js
@@ -9,91 +9,14 @@ export default function TodoBody() {
 			<div className={styles.listItems}>
 				<div className={styles.todoCard}>
 					<div className={styles.cardHeader}>
-						{/* <input
-							className={styles.inputEdit}
-							type="text"
-							placeholder="Cooking"
-						/> */}
 						<p className={styles.cardTitle}>Cooking</p>
-						<i className="fa-solid fa-pen"></i>
+						<i className="icon fa-solid fa-pen"></i>
 					</div>
 
 					<div className={styles.cardBody}>
 						<div className={styles.categoryField}>
-							<CategoryInTodoCard />
-
 							<div className={styles.categoryContainer}>
-								<div className={styles.category}>
-									Life <i className="fa-solid fa-x"></i>
-								</div>
-							</div>
-						</div>
-
-						<div className={styles.date}>
-							<p>Start Date: </p>
-							<p>Finished Date: </p>
-						</div>
-
-						<div className={styles.cardFooter}>
-							<i className="icon--trash fa-solid fa-trash-can"></i>
-							<input className={styles.cardCheckbox} type="checkbox" />
-						</div>
-					</div>
-				</div>
-
-				<div className={styles.todoCard}>
-					<div className={styles.cardHeader}>
-						{/* <input
-							className={styles.inputEdit}
-							type="text"
-							placeholder="Cooking"
-						/> */}
-						<p className={styles.cardTitle}>Cooking</p>
-						<i className="fa-solid fa-pen"></i>
-					</div>
-
-					<div className={styles.cardBody}>
-						<div className={styles.categoryField}>
-							<CategoryInTodoCard />
-
-							<div className={styles.categoryContainer}>
-								<div className={styles.category}>
-									Life <i className="fa-solid fa-x"></i>
-								</div>
-							</div>
-						</div>
-
-						<div className={styles.date}>
-							<p>Start Date: </p>
-							<p>Finished Date: </p>
-						</div>
-
-						<div className={styles.cardFooter}>
-							<i className="icon--trash fa-solid fa-trash-can"></i>
-							<input className={styles.cardCheckbox} type="checkbox" />
-						</div>
-					</div>
-				</div>
-
-				<div className={styles.todoCard}>
-					<div className={styles.cardHeader}>
-						{/* <input
-							className={styles.inputEdit}
-							type="text"
-							placeholder="Cooking"
-						/> */}
-						<p className={styles.cardTitle}>Cooking</p>
-						<i className="fa-solid fa-pen"></i>
-					</div>
-
-					<div className={styles.cardBody}>
-						<div className={styles.categoryField}>
-							<CategoryInTodoCard />
-
-							<div className={styles.categoryContainer}>
-								<div className={styles.category}>
-									Life <i className="fa-solid fa-x"></i>
-								</div>
+								<div className={styles.category}>Life</div>
 							</div>
 						</div>
 
